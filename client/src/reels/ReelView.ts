4916f0d8ef464cc2fdc6def0285b6154b1b3d465
Yahor Padlucki <yapagames@gmail.com>
2017-08-28 23:15:37 +0300

    making reel spinning

--- client/src/reels/ReelView.ts
+++ client/src/reels/ReelView.ts
@@ -17,5 +17,21 @@ export class ReelView extends Container {
 
             this.addChild(symbol);
         }
+        this.startSpin();
+    }
+
+    //TODO: start, stop anim with tweenLite
+    // main anim on pixi draw
+    //reel model
+    public startSpin(): void {
+
+        TweenLite.killTweensOf(this);
+        TweenLite.to(
+            this,
+           5,
+            {
+                y:400
+            }
+        );
     }
 }
