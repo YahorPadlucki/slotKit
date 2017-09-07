8f1195d837982ff6f08cc755f513c68b395ef9e4
Yahor Padlucki <yahor.padlucki@skywindgroup.com>
2017-09-07 08:35:11 +0300

    making reel spin

--- client/src/reels/ReelView.ts
+++ client/src/reels/ReelView.ts
@@ -9,8 +9,6 @@ export class ReelView extends Container {
     private rows: number = 5;
 
     private symbols: SymbolView[] = [];
-    private topSymbolVisible: SymbolView;
-    private topSymbol: SymbolView;
 
     constructor() {
         super();
@@ -25,20 +23,14 @@ export class ReelView extends Container {
     private initReels() {
         this.addVisibleSymbols();
 
-        //TODO: remake order dependence
-        this.addSymbolToBottom();
-        this.addSymbolToTop();
+        this.addNonVisibleSymbolToTop();
+        this.addNonVisibleSymbolToBottom();
     }
 
     private addVisibleSymbols() {
         for (let i = 0; i < this.rows; i++) {
             const symbol = new SymbolView(0xFF3300);
 
-            if (i === 0) {
-                this.topSymbolVisible = symbol;
-                this.topSymbol = symbol;
-
-            }
             symbol.y = symbol.symbolHeight * i + this.verticalGap * i;
             this.symbols.push(symbol);
             this.addChild(symbol);
@@ -51,10 +43,9 @@ export class ReelView extends Container {
 
     private spin(): void {
         this.symbols.forEach((symbol) => symbol.y++);
-        console.log(this.topSymbolVisible.y)
-        if (this.topSymbolVisible.y > this.topSymbolVisible.symbolHeight) {
-            this.topSymbolVisible = this.symbols[this.symbols.length-1];// todo:remake to list las?
-            this.topSymbol = this.addSymbolToTop();
+        const topSymbol = this.symbols[0];
+        if (topSymbol.y >= -topSymbol.symbolHeight) {
+            this.addNonVisibleSymbolToTop();
         }
     }
 
@@ -73,21 +64,22 @@ export class ReelView extends Container {
         );
     }
 
-    private addSymbolToTop() {
-        const bottomAdditionalSymbol = new SymbolView(0x003300);
-        bottomAdditionalSymbol.y = this.topSymbol.y - this.verticalGap - bottomAdditionalSymbol.symbolHeight;
-        this.addChild(bottomAdditionalSymbol);
-        this.symbols.push(bottomAdditionalSymbol);
-        return bottomAdditionalSymbol;
+    private addNonVisibleSymbolToTop() {
+        const topNonVisibleSymbol = new SymbolView(0x003300);
+        const topSymbol = this.symbols[0];
 
-
+        topNonVisibleSymbol.y = topSymbol.y - this.verticalGap - topNonVisibleSymbol.symbolHeight;
+        this.addChild(topNonVisibleSymbol);
+        this.symbols.unshift(topNonVisibleSymbol);
     }
 
-    private addSymbolToBottom() {
-        const topAdditionalSymbol = new SymbolView(0x003300);
-        topAdditionalSymbol.y = this.topSymbolVisible.y + (this.verticalGap * this.rows - 1) + (this.topSymbolVisible.height * this.rows);
-        this.addChild(topAdditionalSymbol);
-        this.symbols.push(topAdditionalSymbol);
+    private addNonVisibleSymbolToBottom() {
+        const bottomNonVisibleSymbol = new SymbolView(0x003300);
+        const topSymbol = this.symbols[0];
+
+        bottomNonVisibleSymbol.y = topSymbol.y + (this.verticalGap * this.symbols.length - 1) + (topSymbol.height * this.symbols.length);
+        this.addChild(bottomNonVisibleSymbol);
+        this.symbols.push(bottomNonVisibleSymbol);
 
     }
 }
